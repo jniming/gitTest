fc2611fecb2508bbddcd32bfcc6fef019a7972c0
245545357@qq.com <245545357@qq.com>
2017-09-22 12:36:20 +0800

    删除了一些东西

--- GitObjectDemo/ViewController.m
+++ GitObjectDemo/ViewController.m
@@ -17,8 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"test");
-    NSLog(@"test2222");
+  
     NSLog(@"分支2的修改");
 }
 
